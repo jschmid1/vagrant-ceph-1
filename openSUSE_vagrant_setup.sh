710063dc063dbce10e3906ed08d99c3e80dabbb1
Joshua Schmid <jschmid@suse.de>
2018-01-30 11:15:09 +0100

    modernize zypper flags and force issue resolution
    Signed-off-by: Joshua Schmid <jschmid@suse.de>

--- openSUSE_vagrant_setup.sh
+++ openSUSE_vagrant_setup.sh
@@ -1,10 +1,10 @@
 set -ex
 
 #install vagrant and it dependencies, devel files to build vagrant plugins later
-zypper in -y https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.rpm
-zypper in -y ruby-devel
-zypper in -y gcc gcc-c++ make
-zypper in -y qemu-kvm libvirt-daemon-qemu libvirt libvirt-devel
+zypper -n in https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.rpm
+zypper -n in --force-resolution ruby-devel
+zypper -n in --force-resolution gcc gcc-c++ make
+zypper -n in --force-resolution qemu-kvm libvirt-daemon-qemu libvirt libvirt-devel
 
 #need for vagrant-libvirt
 gem install ffi -v '1.9.18'
@@ -18,5 +18,4 @@ vagrant plugin install vagrant-libvirt
 
 git clone --depth 1 https://github.com/openSUSE/vagrant-ceph
 cd vagrant-ceph
-
 #vagrant up
